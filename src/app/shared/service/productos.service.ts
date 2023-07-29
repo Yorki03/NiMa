8e019444a9b463d80eb26f7ac822749d1bff948c
Rodolfo <rodolvg87@gmail.com>
2023-07-29 20:23:44 +0200

    Ajuste de variables de entorno

--- src/app/shared/service/productos.service.ts
+++ src/app/shared/service/productos.service.ts
@@ -7,7 +7,9 @@ import { Manga } from 'src/app/peges/confeccion/models/manga';
 import { Cuerpo } from 'src/app/peges/confeccion/models/cuerpo';
 import { Tela } from 'src/app/peges/confeccion/models/tela';
 import { Boton } from 'src/app/peges/confeccion/models/boton';
+import { environment } from 'src/environments/environment';
 
+const baseUrl = `${environment.apiUrl}/costo-envio`;
 
 @Injectable({
   providedIn: 'root'
@@ -17,35 +19,35 @@ export class ProductosService {
   constructor(private http: HttpClient) { }
 
   getCuello(): Observable<Cuello[]> {
-    return this.http.get<Cuello[]>(`https://nima-t0xd.onrender.com/cuello`).pipe(
+    return this.http.get<Cuello[]>(`${baseUrl}/cuello`).pipe(
       map((res) => res.map((cuello) => new Cuello(cuello))),
       catchError(handleError)
     );
   };
 
   getManga(): Observable<Manga[]> {
-    return this.http.get<Manga[]>(`https://nima-t0xd.onrender.com/manga`).pipe(
+    return this.http.get<Manga[]>(`${baseUrl}/manga`).pipe(
       map((res) => res.map((manga) => new Manga(manga))),
       catchError(handleError)
     );
   };
 
   getCuerpo(): Observable<Cuerpo[]> {
-    return this.http.get<Cuerpo[]>(`https://nima-t0xd.onrender.com/cuerpo`).pipe(
+    return this.http.get<Cuerpo[]>(`${baseUrl}/cuerpo`).pipe(
       map((res) => res.map((cuerpo) => new Cuerpo(cuerpo))),
       catchError(handleError)
     );
   };
 
   getTelas(): Observable<Tela[]> {
-    return this.http.get<Tela[]>(`https://nima-t0xd.onrender.com/telas`).pipe(
+    return this.http.get<Tela[]>(`${baseUrl}/telas`).pipe(
       map((res) => res.map((tela) => new Tela(tela))),
       catchError(handleError)
     );
   };
 
   getBotones(): Observable<Boton[]> {
-    return this.http.get<Boton[]>(`https://nima-t0xd.onrender.com/botones`).pipe(
+    return this.http.get<Boton[]>(`${baseUrl}/botones`).pipe(
       map((res) => res.map((boton) => new Boton(boton))),
       catchError(handleError)
     );
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://nima-t0xd.onrender.com',
+};
