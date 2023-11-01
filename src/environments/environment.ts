16928ca0c637812924ec3dc1a27645b2a334af21
Yorki03 <110152632+Yorki03@users.noreply.github.com>
2023-11-01 18:48:16 -0400

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://nima-t0xd.onrender.com',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://nima-t0xd.onrender.com',
 };
